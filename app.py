6f0e4fbcb8c1dc68b0bdb86870dd8630ef85f101
Vedaant Dutt <vedaantdutt@gmail.com>
2020-12-18 11:44:26 +0900

    removing empty rows, shuffling

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ import pandas as pd
 data = pd.read_csv("dataset/data.csv")
 print(data.head())
 
+data = data.dropna()
+
+data = data.sample(frac=1, random_state=42)
+print(data.head())
+
+print(data.head())
+print(data.shape)
+
