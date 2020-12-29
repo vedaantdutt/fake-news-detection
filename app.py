2172ab479cf5d688d6ca999a7d2a7d7a6cbf40f1
Vedaant Dutt <vedaantdutt@gmail.com>
2020-12-29 11:54:23 +0900

    nltk libraries added

--- app.py
+++ app.py
@@ -9,6 +9,18 @@ data = data.dropna()
 data = data.sample(frac=1, random_state=42)
 print(data.head())
 
-print(data.head())
 print(data.shape)
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import string
+import re
+
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+
+stop_words = set(stopwords.words('english'))
 
